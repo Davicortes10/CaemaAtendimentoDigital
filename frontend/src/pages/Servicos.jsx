074e862b6a9717a7ef62664fb595aa1622975bf0
FelipeGarcia <engfelipegarcia@hotmail.com>
2025-09-30 18:10:52 -0300

    implementado componente box na pagina de serviço

--- frontend/src/pages/Servicos.jsx
+++ frontend/src/pages/Servicos.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import Layout from '../components/layout/Layout';
 import Logo from '../components/ui/Logo';
+import Box from '../components/ui/Box';
 
 
 const Servicos = () => {
   return (
     <Layout>
       <Logo />
-      <div className="w-full text-center p-20">
-        <h1 className="text-white text-5xl font-extrabold">
-          Tela de Serviços - Em Construção! 
-        </h1>
-        <p className="text-gray-400 text-2xl mt-4">
-          Aguardando o conteúdo.
-        </p>
+      <div className='flex flex-row'>
+      <div >
+        <Box 
+            label={"2ª Via da Conta"}
+            onClick={() => {}}
+            type="submit"/>
+      </div>
+      <div >
+        <Box 
+            label={"Informar Falta de Água"}
+            onClick={() => {}}
+            type="submit"/>
+      </div>
+      <div >
+        <Box 
+            label={"Alterar Dados Cadastrais"}
+            onClick={() => {}}
+            type="submit"/>
+      </div>
+      <div>
+        <Box 
+            label={"Comunicar Vazamento"}
+            onClick={() => {}}
+            type="submit"/>
+      </div>
       </div>
     </Layout>
   );
