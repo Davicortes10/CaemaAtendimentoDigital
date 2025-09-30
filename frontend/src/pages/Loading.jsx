01597d0951f548d7f09d5b254e1ecf74e4c78f0f
isgust <rocha.gustavops@gmail.com>
2025-09-30 14:21:42 -0300

    Navigate in Loading page, div for aninamate loading and titles h2,h3

--- frontend/src/pages/Loading.jsx
+++ frontend/src/pages/Loading.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React,{ lazy, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+// Components
 import Layout from '../components/layout/Layout';
 import Logo from '../components/ui/Logo';
 
 
 const Loading = () => {
+  const navigate = useNavigate(); 
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            // Next page if executed with sucess
+            navigate('/endereco'); 
+        }, 3000);
+        
+        return () => clearTimeout(timer);
+    // Navigate in array of dependencies
+    }, [navigate]); 
   return (
     <Layout>
       <Logo />
-      <div className="w-full text-center p-20">
-        <h1 className="text-white text-5xl font-extrabold">
-          Tela de Loading - Em Construção! 
-        </h1>
-        <p className="text-gray-400 text-2xl mt-4">
-          Aguardando o conteúdo.
-        </p>
-      </div>
+      <div className="animate-[spin_3s_linear_infinite]
+                            rounded-full 
+                            h-58 w-58
+                            border-[20px]       /* Largura da borda/anel (20px) */
+                          border-white     
+                          border-t-lime-600
+                          border-l-lime-600
+                          pb-10"
+                          
+                          
+            ></div>
+            <h2 className="text-white text-4xl pt-7">Validando seus dados...</h2>
+            <h3 className="text-white">Buscando dados no sistema</h3>
     </Layout>
   );
 };
